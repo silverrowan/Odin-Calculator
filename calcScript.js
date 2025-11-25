b23a9fb22735570c16fdade7ed38c60dfd665d0f
SilverRowan <silverrowan@gmail.com>
2025-11-24 21:11:27 -0800

    Create Math Functions & Selector
    Created a function for each basic math formula (addNumbers(), subtractNumbers(), multiplyNumbers(), and divideNumbers() ), and created a switch function: evaluateCalculation(), which directs the requests to the correct Math Function.
    Deleted "hello" test.

--- calcScript.js
+++ calcScript.js
@@ -1,1 +1,37 @@
-console.log("hello");
+let userNum1;
+let userNum2;
+let userOperator;
+let displayEquation;
+let displayAnswer;
+
+function evaluateCalculation(userNum1, userNum2, userOperator) {
+    switch (userOperator) {
+        case '+' :
+            return displayAnswer = addNumbers(userNum1, userNum2);
+        case '-' :
+            return displayAnswer = subtractNumbers(userNum1, userNum2);
+        case '*' :
+            return displayAnswer = multiplyNumbers(userNum1, userNum2);
+        case '/' :
+            return displayAnswer = divideNumbers(userNum1, userNum2);
+    };
+};
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+};
+function subtractNumbers(num1, num2) {
+    return num1 - num2;
+};
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+};
+function divideNumbers(num1, num2) {
+    if (num2 === 0) {
+        return 'Impossible';
+    } else {
+        return num1 / num2;
+    }
+};
+
+console.log(evaluateCalculation(4,2,'/') )
