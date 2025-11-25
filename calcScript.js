ca592221870a50bd18d5a3f1f415d11e9e9a7d46
SilverRowan <silverrowan@gmail.com>
2025-11-24 22:21:28 -0800

    See button presses in the calc display
    Created function updateDisplay. Created
    directButtonVales, which currently assigns the
    button to display and calls updateDisplay. To be
    reworked to properly direct button presses as needed.
    Reworked evaluateCalculation to call updateDisplay
    every time it runs.

--- calcScript.js
+++ calcScript.js
@@ -6,39 +6,62 @@ let displayAnswer;
 
 const calculatorBodyDiv = document.querySelector('div.frame');
 const displayEquationDiv = calculatorBodyDiv.querySelector('div#equation');
-const displayAnserDiv = calculatorBodyDiv.querySelector('div.vcenter');
-const buttons = calculatorBodyDiv.querySelectorAll('button')
+const displayAnswerDiv = calculatorBodyDiv.querySelector('div.vcenter');
+const buttons = calculatorBodyDiv.querySelectorAll('button');
+const displayAnswerContainer = document.querySelector('.display.answer')
+console.log(buttons);
 
 buttons.forEach( (button) => {
-    button.addEventListener( "click", () => console.log(`click ${button.textContent}`) );
+    button.addEventListener( "click", (e) => directButtonValues(e) );
 });
 
-function evaluateCalculation(userNum1, userNum2, userOperator) {
+function directButtonValues(e) {
+    displayAnswer = e.target.textContent;
+    updateDisplay(equation, displayAnswer);
+};
+
+function updateDisplay(equation, answer) {
+    displayEquationDiv.textContent = equation;
+    displayAnswerDiv.textContent = answer;
+    displayAnswerContainer.appendChild(displayAnswerDiv);
+    calculatorBodyDiv.prepend(displayAnswerContainer);
+    calculatorBodyDiv.prepend(displayEquationDiv);
+}
+
+function evaluateCalculation(userNum1, userNum2, userOperator, equation) {
     switch (userOperator) {
         case '+' :
-            return displayAnswer = addNumbers(userNum1, userNum2);
+            displayAnswer = addNumbers(userNum1, userNum2);
+        break;
         case '-' :
-            return displayAnswer = subtractNumbers(userNum1, userNum2);
+            displayAnswer = subtractNumbers(userNum1, userNum2);
+        break;
         case '*' :
-            return displayAnswer = multiplyNumbers(userNum1, userNum2);
+            displayAnswer = multiplyNumbers(userNum1, userNum2);
+        break;
         case '/' :
-            return displayAnswer = divideNumbers(userNum1, userNum2);
+            displayAnswer = divideNumbers(userNum1, userNum2);
+        break;
     };
+    updateDisplay(equation, displayAnswer);
+    return displayAnswer;
 };
 
 function addNumbers(num1, num2) {
-    return num1 + num2;
+    return displayAnswer = num1 + num2;
 };
 function subtractNumbers(num1, num2) {
-    return num1 - num2;
+    return displayAnswer = num1 - num2;
 };
 function multiplyNumbers(num1, num2) {
-    return num1 * num2;
+    return displayAnswer = num1 * num2;
 };
 function divideNumbers(num1, num2) {
     if (num2 === 0) {
-        return 'Impossible';
+        return displayAnswer = 'Impossible';
     } else {
-        return num1 / num2;
+        return displayAnswer = num1 / num2;
     }
 };
+
+// console.log(evaluateCalculation (1,2,'+','1+2'));
